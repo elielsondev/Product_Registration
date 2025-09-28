c22b1705ba76a3086434050db938caec8e2f2440
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-09-28 20:38:49 -0300

    feat - Implementing the event function to save data to localStorage, clear inputs, and display an alert when fields are not filled in.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
 console.log("Running, Product Registration!");
+
+const formLogin = document.getElementById("form-login");
+
+const userName = document.getElementById("username");
+const email = document.getElementById("email");
+
+const btnEnter = document.getElementById("btn-enter");
+
+
+btnEnter.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (localStorage.key("dataLogin")) {
+    localStorage.removeItem("dataLogin");
+  }
+
+  if (userName.value === "" || email.value === "") {
+    alert("Por favor, preencha os campos com nome e e-mail para poder logar!");
+    return;
+  }
+
+  const dataLogin = { name:userName.value, email:email.value };
+  console.log(dataLogin);
+  
+  const register = JSON.parse(localStorage.getItem("dataLogin")) || [];
+
+  register.push(dataLogin);
+  console.log(register);
+  
+  userName.value = "";
+  email.value = "";
+});
