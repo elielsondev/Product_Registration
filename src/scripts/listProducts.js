ec3a856f03b1cfe31eaa7ee45ef91965193498ab
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-09-29 01:55:25 -0300

    feat - user logout event redirecting to the login page, and redirection event to the product registration page

--- src/scripts/listProducts.js
+++ src/scripts/listProducts.js
@@ -3,9 +3,21 @@ const emailStorage = document.getElementById("email-storage");
 
 function recoveryDataLogin () {
     const { name, email } = JSON.parse(localStorage.getItem("dataLogin"))[0];
-
     nameStorage.innerHTML = `Olá, seja bem-vindo(a): ${name}`;
     emailStorage.innerHTML = email;
 };
 
 recoveryDataLogin();
+
+const btnLogout = document.getElementById("btn-logout");
+btnLogout.addEventListener("click", (e) => {
+    e.preventDefault();
+    localStorage.removeItem("dataLogin");
+    window.location.href = "/index.html";
+});
+
+const btnPageRegisterNewProduct = document.getElementById("page-register-new-product");
+btnPageRegisterNewProduct.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/pages/addProducts.html";
+});
