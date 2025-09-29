565e900a0b98ae87ceda742965a85a2b97dc42e1
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-09-28 21:20:47 -0300

    fix - setting the item in dataLogin in localStorage

--- src/main.js
+++ src/main.js
@@ -11,22 +11,20 @@ const btnEnter = document.getElementById("btn-enter");
 btnEnter.addEventListener("click", (e) => {
   e.preventDefault();
 
-  if (localStorage.key("dataLogin")) {
-    localStorage.removeItem("dataLogin");
-  }
+  if (localStorage.key("dataLogin")) { localStorage.removeItem("dataLogin"); };
 
   if (userName.value === "" || email.value === "") {
     alert("Por favor, preencha os campos com nome e e-mail para poder logar!");
     return;
   }
-
+  
   const dataLogin = { name:userName.value, email:email.value };
-  console.log(dataLogin);
   
   const register = JSON.parse(localStorage.getItem("dataLogin")) || [];
-
+  
   register.push(dataLogin);
-  console.log(register);
+  
+  localStorage.setItem("dataLogin", JSON.stringify(register));
   
   window.location.href = "./src/pages/addProducts.html";
   
