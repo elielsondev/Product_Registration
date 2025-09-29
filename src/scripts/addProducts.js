7aced88dd52952e0a220373dfcbaa67b65f72cbb
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-09-29 01:47:58 -0300

    feat - event for the logout button that logs out the user and directs them to the login page, and event for directing them to the registered products page

--- src/scripts/addProducts.js
+++ src/scripts/addProducts.js
@@ -3,9 +3,21 @@ const emailStorage = document.getElementById("email-storage");
 
 function recoveryDataLogin () {
     const { name, email } = JSON.parse(localStorage.getItem("dataLogin"))[0];
-
     nameStorage.innerHTML = `Olá, seja bem-vindo(a): ${name}`;
     emailStorage.innerHTML = email;
 };
 
 recoveryDataLogin();
+
+const btnLogout = document.getElementById("btn-logout");  
+btnLogout.addEventListener("click", (e) => {
+    e.preventDefault();
+    localStorage.removeItem("dataLogin");
+    window.location.href = "/index.html";
+});
+
+const btnPageViewRegisteredProducts = document.getElementById("page-view-registered-products");
+btnPageViewRegisteredProducts.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/src/pages/listProducts.html";
+});
